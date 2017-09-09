455a6a9ee0c3b2dddb0c39d51170465d8981d522
bkim121 <bkim121@gmail.com>
2017-09-09 15:11:24 -0700

    Impleneted app changing, videoplayer plays correct video from videoentrylist, pass all test up to app

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,11 +1,14 @@
-var VideoList = (props) => (
-  <div className="video-list">
-      {props.videos.map(item =>
-        <VideoListEntry video={item}/>
-      )}
-  </div>
-);
+var VideoList = (props) => {
 
+
+  return (
+    <div className="video-list">
+        {props.videos.map(item =>
+          <VideoListEntry click={props.onClick} video={item} />
+        )}
+    </div>
+  );
+};
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
--- src/components/VideoListEntry.jsx
+++ src/components/VideoListEntry.jsx
@@ -16,7 +16,7 @@
 //           <img className="media-object" src={this.props.thumbnail} alt="" />
 //         </div>
 //       <div className="media-body">
-//         <div className="video-list-entry-title">{this.props.title}</div>
+//         <div onClick={this.props.click.bind(this)} className="video-list-entry-title">{this.props.title}</div>
 //           <div className="video-list-entry-detail">{this.props.description}</div>
 //         </div>
 //       </div>
@@ -24,19 +24,23 @@
 //   }
   
 // }
-//title={item.snippet.title} description={item.snippet.description} thumbnail={item.snippet.thumbnails.default.url}
-var VideoListEntry = (props) => (
+// title={item.snippet.title} description={item.snippet.description} thumbnail={item.snippet.thumbnails.default.url}
 
+
+var VideoListEntry = (props) => {
+
+  return (
       <div className="video-list-entry media">
         <div className="media-left media-middle">
           <img className="media-object" src={props.video.snippet.thumbnails.default.url} alt="" />
         </div>
         <div className="media-body">
-          <div className="video-list-entry-title">{props.video.snippet.title}</div>
+          <div onClick={function() { props.click(props.video.id.videoId); } } className="video-list-entry-title">{props.video.snippet.title}</div>
           <div className="video-list-entry-detail">{props.video.snippet.description}</div>
         </div>
       </div>
-);
+  );
+};
 
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
@@ -47,3 +51,5 @@ VideoListEntry.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 window.VideoListEntry = VideoListEntry;
+
+//onClick={this.onItemClick.bind(this)} 
